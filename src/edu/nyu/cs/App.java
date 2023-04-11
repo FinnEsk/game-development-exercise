9b11d948fe831264a247880c09a21226b1bef0dc
FinnEsk <fee9992@nyu.edu>
2023-04-11 17:09:58 -0400

    1.3

--- src/edu/nyu/cs/App.java
+++ src/edu/nyu/cs/App.java
@@ -1,12 +1,14 @@
 package edu.nyu.cs;
 
+import java.util.Random;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import org.apache.commons.lang3.SystemUtils;
 
-import processing.core.*; 
-import processing.sound.*;
+import processing.core.PApplet;
+//import processing.core.*; 
+//import processing.sound.*;
 
 
 
@@ -16,8 +18,8 @@ public class App extends PApplet {
 
 
     private Block block;
-    private Obstacle obstacle;
-
+    
+private Obstacle obstacle;
     
     public void settings(){
         this.size(600, 600);
@@ -25,15 +27,18 @@ public class App extends PApplet {
 
     public void setup(){
 
-        this.block = new Block(100, 100, this);
-        this.obstacle = new Obstacle(100, 100, this);
+        this.block = new Block(300, 250, this);
+        this.obstacle = new Obstacle(500, 275, this);
 
     }
 
     public void draw(){
         this.background(0, 0, 0);
         this.noFill();
-        this.line(0,150,600,150);
+
+        
+        this.stroke(100,100,100);
+        this.line(0,300,600,300);
 
         this.block.draw();
         this.obstacle.draw();
@@ -43,6 +48,10 @@ public class App extends PApplet {
     public void keyPressed(){
         block.jump();
         block.draw();
+    }
+
+    public void mouseClicked(){
+        block.randomColor();
     }
 
     public static void main(String[] args){
--- src/edu/nyu/cs/Block.java
+++ src/edu/nyu/cs/Block.java
@@ -1,27 +1,58 @@
 package edu.nyu.cs;
 
+import java.util.Random;
 
 public class Block {
+
     
 
-    private int x = 200;
-    private int y = 150;
-    private int xspeed = 2;
-    private int yspeed = 2;
+    private int x = 300;
+    private int y = 300;
     private App app;
+    private int r;
+    private int g;
+    private int b;
+    private boolean isJumping;
 
     public Block(int x, int y, App app){
         this.x = x;
         this.y = y;
         this.app = app;
+        this.r = 0;
+        this.g = 150;
+        this.b = 150;
     }
 
     public void draw(){
         this.app.rect(this.x, this.y, 50, 50);
+        this.app.stroke(this.r, this.g, this.b);
+        
+        
+        if(isJumping == true){
+            y--;
+            if(y < 200){
+                isJumping = false;
+            }
+        }
+        else if(this.y < 250){
+            y++;
+        }
+        
     }
 
     public void jump(){
-        this.y = y + yspeed;
+        isJumping = true;
         // should I do a timed loop somehow?
+    }
+
+    public int randomizer(){
+        Random rand = new Random();
+        return rand.nextInt(255);
+    }
+
+    public void randomColor(){
+        this.r = randomizer();
+        this.g = randomizer();
+        this.b = randomizer();
     }
 } 
